603580f16cdcab484e42167e46aae91f077f81a8
tbhuabi <464328895@qq.com>
2023-07-08 21:17:29 +0800

    feat: 更新 viewfly 版本

---
+++ jsx.d.ts
@@ -0,0 +1,9 @@
+import { HTMLAttributes } from '@viewfly/platform-browser'
+
+declare module '@viewfly/core/jsx-runtime' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'tb-alert': HTMLAttributes<HTMLElement>
+    }
+  }
+}
